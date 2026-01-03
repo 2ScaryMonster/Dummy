2a9f85aa26b73ef9e2f3e31d4ef6a2d27954b85b
KUBZ <kubz@dead-pc.dead-pc.com>
2026-01-03 23:30:59 +0530

    Add project files

---
+++ scr.js
@@ -0,0 +1,7 @@
+function addno()
+{
+    let a=Number(document.getElementById("n1").value);
+    let b=Number(document.getElementById("n2").value);
+    let c=a+b;
+    document.getElementById("rs").innerText = c;
+}
